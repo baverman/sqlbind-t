746c7948a5d1f1005b0a264f68f6b6b3b30a3a5a
Anton Bobrov <baverman@gmail.com>
2026-02-12 11:53:48 +0000

    keep image hashes in context directory

--- ci/run.sh
+++ ci/run.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 set -e
 
-thisdir="$(dirname "$0")"
 ctx="$1"
 shift
 
 ver="$(basename "$ctx")"
-image_hash_file="$thisdir/image.$ver.hsh"
+image_hash_file="$ctx/image.hsh"
 
 if [ -n "$FORCE_IMAGE" -o ! -f "$image_hash_file" ]; then
     docker build --iidfile "$image_hash_file" "$ctx"
